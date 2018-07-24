b8ea4e567f9850a77870b2bc065148f95217a517
Fabiano Beselga <fabianobeselga@gmail.com>
2018-07-24 09:46:14 -0300

    refactor: Move the state of bookshelf to BookApp
    Mananing the state of books on the BookApp component.
    Also books array is now a props of Bookshelf.
    For now I think that this reflects more the architecture needed.

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,24 +1,27 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types';
 import Book from './Book'
 
 class BookShelf extends Component {
-  state = {
-    books: [
-      { title: "To kill a Mocking Bird",
-        coverURL: 'http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api',
-        authors: 'Harper Lee' }
-    ]
-  }
 
   render() {
     return (
-      <ol>
-        {this.state.books.map((book) => (
-          <li key={book.title}><Book {...book}/></li>
-        ))}
-      </ol>
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{this.props.status}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {this.props.books.map((book) => (
+              <li key={book.title}><Book {...book}/></li>
+            ))}
+          </ol>
+        </div>
+      </div>
     )
   }
+}
+
+BookShelf.propTypes = {
+  books: PropTypes.array.isRequired
 }
 
 export default BookShelf
